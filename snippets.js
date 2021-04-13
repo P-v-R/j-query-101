8902503adb0da483f9f8d5ffff41d8749745a683
Perryvon <Perry.j.vr@gmail.com>
2021-04-12 17:38:03 -0700

    finished part one of dom challenge

---
+++ snippets.js
@@ -0,0 +1,21 @@
+// 2)  $("img").addClass("image-center");
+
+// 3)  let paragraphs = $("P");
+//     paragraphs[paragraphs.length-1].remove();
+
+// 4)  $("#title").css("font-size", `${Math.random()*100}px`);
+
+// 5) listItems.append("<li> testing testing!</li>");
+
+//  6) let listItems = $("ol");
+//     listItems.remove();
+//     $("aside").append("<p>please forgive me for that stupid list</p>");
+
+
+//  7)  $(".form-control").on("change", function(){
+//     let red = $(".form-control").eq(0).val();
+//     let blue = $(".form-control").eq(1).val();
+//     let green = $(".form-control").eq(2).val();
+//     $("body").css("background-color", `rgb(${red},${blue},${green})`);
+
+//   })
